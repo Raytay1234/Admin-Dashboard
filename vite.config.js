140857f42fc2122457fea2fdc836bd77f5449ea6
Raytay1234 <ryanmugi2004@gmail.com>
2026-02-10 09:39:26 +0300

    fixes 2

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-
   server: {
+    // Fix 404 on page refresh for React Router
+    historyApiFallback: true,
+
+    // Optional: your API proxy
     proxy: {
       "/api": {
         target: "http://localhost:5000",
